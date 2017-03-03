f4960142c259fa8665a10ed8ae671043662203d3
Ian Matthew Morey <imorey@OEM.NYCNET>
2017-03-03 14:43:50 -0500

    Copied files from census explorer project

---
+++ js/map.js
@@ -0,0 +1,23 @@
+var map;
+
+map	= new L.map('map-container', { zoomControl:false }).setView([40.7153604,-73.9837349], 11);
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {attribution: 'Positron'}).addTo(map);
+
+var allLayers = 'https://nycem.carto.com/u/imorey/api/v2/viz/217510a8-447a-11e6-81f4-0e05a8b3e3d7/viz.json';
+
+
+cartodb.createLayer(map, allLayers, {https: true})
+	.addTo(map)
+	.on('done', function(layer) {
+		censusTract = layer.getSubLayer(0);
+		//censusTract.setInteraction(false);
+
+		nTabArea = layer.getSubLayer(1); 
+		//nTabArea.hide();  //hide neighborhood polygons
+		//nTabArea.on('featureClick', processNeighborhood);
+	})
+	.on('error', function() {
+    //log the error
+	alert("some error occurred: " + err);
+    });
